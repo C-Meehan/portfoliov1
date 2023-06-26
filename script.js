97d579188390fe8b379846db7de0de1767053d82
Christopher Meehan <chrismeehan91@gmail.com>
2023-06-26 18:19:57 -0400

    started portfolio

---
+++ script.js
@@ -0,0 +1,49 @@
+// $(window).scroll(function () {
+//     // 100 = The point you would like to fade the nav in.
+
+//     if ($(window).scrollTop() > 100) {
+
+//         $('.navbar').addClass('show');
+
+//     } else {
+
+//         $('.navbar').removeClass('show');
+
+//     };
+// });
+// var typed = new Typed(".typing", {
+//     strings: ["Web Developer", "Mentor", "Open Source Contributor", "Action on Google Developer", "Freelancer", "AI Enthusiast", "Front End Enthusiast", "UI Designer", "Photographer", "Technical Writer", "Author", "Cloud Computing Enthusiast"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
