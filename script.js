181ee576fd9de0767f4bc796dfeb9752d4e3213b
Christopher Meehan <chrismeehan91@gmail.com>
2023-08-11 20:31:42 -0400

    added success page for reroute on email. need to fix toggle

--- script.js
+++ script.js
@@ -65,5 +65,16 @@ function toggleModal() {
   document.body.classList += " modal--open"
 }
 
+function toggleModalSuccess() {
+  if (isModalOpen) {
+    isModalOpen = false
+    return document.body.classList.remove('modal--open')
+
+  }
+  isModalOpen = true
+  document.body.classList += " modal--open"
+}
+
+toggleModalSuccess();
 
 // service_s13lxyz
